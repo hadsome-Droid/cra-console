86abc884b2920ddff2c4db984a7dc958595db171
hadsome-Droid <xkumbhax@gmail.com>
2023-08-11 19:09:42 +0300

    React Component Complite

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-          Lorem ipsum dolor!!!!9299292
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    return (
+        <div>
+            'This is APP componet' 124 32 13 4
+            <Rating/>
+            <Accordion/>
+        </div>
+
+    );
+}
+
+function Rating() {
+    return (
+        <div>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+            <Star/>
+        </div>
+    );
+}
+
+function Accordion() {
+    return <div>
+        <ul>
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+        </ul>
     </div>
-  );
+}
+
+function Star() {
+    return <div>
+        <div>Star</div>
+    </div>
 }
 
 export default App;
